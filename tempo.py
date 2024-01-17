1694804196b303357d9cc0485f901e11bbe8f00d
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2024-01-17 13:59:43 +0100

    Update tempo.py

--- tempo.py
+++ tempo.py
@@ -68,4 +68,4 @@ def index():
     return render_template('stock_analysis.html', stock_data=stock_data)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
